276f08ce44995cb683563a17df6f7834c681e308
JaanuGopan <sjanugopan@gmail.com>
2023-12-07 14:56:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterlec1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterlec1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('Flutter Demo'),
+      ),
+      body: Container(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
